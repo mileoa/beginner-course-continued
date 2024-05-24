847467a8bbe9903d9da695d5a136bdb458750c5a
mileoa <ezkimozzzzz@gmail.com>
2024-05-24 22:05:32 +1000

    add asserts

---
+++ Отладка и логирование/task1.py
@@ -0,0 +1,118 @@
+import random
+
+class Robot:
+    
+    def __init__(self, max_health, health, max_speed, x, y, temperature):
+        self.__max_health = max_health # Максимальное здоровье
+        self.__health = health # Здоровье
+        self.__max_speed = max_speed # Максимальная скорость робота
+        self.__speed = 0 # Скорость робота
+        self.__noise = 0 # текущий шум робота
+        self.__temperature = temperature
+        self.__x = x # Положение в пространстве по оси x
+        self.__y = y # Положение в пространстве по оси y
+
+    def Get_x(self):
+        return self.__x
+
+    def Get_y(self):
+        return self.__y
+
+    def Get_temperature(self):
+        return self.__temperature
+
+    def Get_noise(self):
+        return self.__noise
+
+    def Get_health(self):
+        return self.__health
+
+    def Run(self, new_speed):
+        if new_speed <= 0:
+            return
+        elif new_speed >= self.__max_speed:
+            self.__speed = self.__max_speed
+        else:
+            self.__speed = new_speed
+        self.__noise += 100
+        self.__temperature += 50
+        assert self.__noise >= 0
+        assert self.__speed > 0
+        assert self.__temperature >= 0 and self.__temperature <=1000
+        
+    def Stop(self):
+        self.__speed = 0
+        self.__noise -= 100
+        self.__temperature -= 50
+        assert self.__speed = 0
+        assert self.__speed > 0
+        assert self.__temperature >= 0 and self.__temperature <=1000
+        
+    def Die(self):
+        self.Stop()
+        self.__health = 0
+        assert self.__health == 0
+
+    def Move_to(self, new_x, new_y):
+        if self.__speed == 0:
+            return
+        self.__x = new_x
+        self.__y = new_y
+  
+    def Increase_health(self, inc_value):
+        if inc_value <= 0:
+            return
+        elif self.__health + inc_value >= self.__max_health:
+            self.__health = self.__max_health
+        else:
+            self.__health += inc_value
+        assert self.__health > 0
+
+    def Decrease_health(self, dec_value):
+        if dec_value <= 0:
+            return
+        elif self.__health == 0:
+            return
+        elif self.__health - dec_value <= 0:
+            self.Die()
+        else:
+            self.__health -= dec_value
+        assert self.__health >= 0 
+
+class Healer(Robot):
+    
+    def __init__(self, max_health, health, max_speed, x, y, temperature,
+                 heal_power, resurrect_ablility):
+        super().__init__(max_health, health, max_speed, x, y, temperature)
+        self.__heal_power = heal_power # Сила лечения
+        self.__resurrect_ablility = resurrect_ablility # Количество доступных воскрешений
+        
+    def Heal(self, target):
+        if instanceof(target, type(None)) or target.Get_health() == 0:
+            return
+        target.Increase_health(self.__heal_power)
+
+    def Resurrect(self, target):
+        if (target.Get_health() != 0 or self.__resurrect_ablility == 0 or
+                instanceof(target, type(None))):
+            return
+        target.Increase_health(self.__heal_power)
+    
+class Destroyer(Robot):
+
+    def __init__(self, max_health, health, max_speed, x, y, temperature,
+                attack_power):
+        super().__init__(max_health, health, max_speed, x, y, temperature)
+        self.__attack_power = attack_power # Сила атаки
+
+    def Attack(self, target):
+        if instanceof(target, type(None)):
+            return
+        target.Decrease_health(self.__attack_power)
+
+    def Can_kill_one_shot(self, target):
+        if instanceof(target, type(None)):
+           return
+        return target.Get_health() <= self.__attack_power
+        
+
