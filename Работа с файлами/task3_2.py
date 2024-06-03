a5025c557d9bfe39294473af3886c904d3e34a02
mileoa <ezkimozzzzz@gmail.com>
2024-06-03 21:38:50 +1000

    Добавил анотации типов и изменил логику возврата результата вычислений.

--- Работа с файлами/task3_2.py
+++ Работа с файлами/task3_2.py
@@ -1,33 +1,39 @@
 import random
+from typing import IO
 
-numbers = []
+def sum_numbers_from_file(fi: IO) -> list:
+    try:
+        file_sum: list = [0, 0]
+        line: str = fi.readline()
+        line_count: int = 0
+        while line != '':
+            file_sum[0] += int(line.rstrip())
+            line = fi.readline()
+            line_count += 1
+        if line_count != 3:
+            return [0, 2]
+        return file_sum
+    except Exception:
+        return [0, 1]
+
+def sum_numbers_from_files(file_names: list, path: str) -> list:
+    try:
+        files_sum: list = [0, 0]
+        for i in file_names:
+            with open(path + str(i) + ".txt", 'r', -1, "utf-8") as fi:
+                sum_file: list = sum_numbers_from_file(fi)
+            if sum_file[1] != 0:
+                return [0, sum_file[1]]
+            files_sum[0] += sum_file[0]
+        return files_sum
+    except Exception:
+        return [0, 3]
+
+numbers: list = []
 for i in range(random.randint(1, 10)):
     numbers.append(random.randint(1, 10))
 
-path = "./task3_1/"
-
-def sum_numbers_from_file(fi):
-    sum = 0
-    line = fi.readline()
-    line_count = 0
-    while line != '':
-       sum += int(line.rstrip())
-       line = fi.readline()
-       line_count += 1
-    if line_count != 3:
-        raise Exception
-    return sum
-
-def sum_numbers_from_files(file_names, path):
-    try:
-        sum = 0
-        for i in file_names:
-            with open(path + str(i) + ".txt", 'r') as fi:
-                sum += sum_numbers_from_file(fi)
-        return sum
-    except Exception:
-        print("Произошла ошибка.")
-        return
+path: str = "./task3_1/"
 
 print(numbers)
-print(sum_numbers_from_files(numbers, path))
+print(sum_numbers_from_files([], path))
