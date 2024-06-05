8e1e40da65f30b6374400950067f535fdbee7300
mileoa <ezkimozzzzz@gmail.com>
2024-06-05 21:30:46 +1000

    Добавил аннотацию типов

--- Дополнение про декораторы и инварианты/task6_1.py
+++ Дополнение про декораторы и инварианты/task6_1.py
@@ -1,19 +1,21 @@
-def disable(func):
-    def wrapper():
+from typing import Callable
+
+def disable(func: Callable) -> Callable:
+    def wrapper() -> None:
         pass
     return wrapper
 
-def use_itself(func):
-    def wrapper(*args):
+def use_itself(func: Callable) -> Callable:
+    def wrapper(*args: int) -> Callable:
         return func(func(*args))
     return wrapper
 
 @disable
-def delete_db():
+def delete_db() -> None:
     print("Всё подчистили.")
 
 @use_itself
-def inc(a):
+def inc(a: int) -> int:
     return a + 1
 
 delete_db()
